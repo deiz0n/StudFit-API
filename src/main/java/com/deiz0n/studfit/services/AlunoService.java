81d654c35118e9a7935741104370351ccabe582f
deiz0n <eduardomacedosilva@outlook.com>
2024-09-06 19:21:17 -0300

    feat: Criação do método getListaDeEspera no AlunoService

--- src/main/java/com/deiz0n/studfit/services/AlunoService.java
+++ src/main/java/com/deiz0n/studfit/services/AlunoService.java
@@ -1,7 +1,11 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.AlunoDTO;
 import com.deiz0n.studfit.repositories.AlunoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class AlunoService {
@@ -10,5 +14,19 @@ public class AlunoService {
 
     public AlunoService(AlunoRepository alunoRepository) {
         this.alunoRepository = alunoRepository;
+    }
+
+    // Retorna todos os alunos que estão na lista de espera
+    public List<AlunoDTO> getListaDeEspera() {
+        return alunoRepository.findAll()
+                .stream()
+                .map(aluno -> AlunoDTO.builder()
+                        .nome(aluno.getNome())
+                        .email(aluno.getEmail())
+                        .colocacao(aluno.getColocacao())
+                        .build()
+                )
+                .filter(aluno -> !aluno.getAtivado())
+                .collect(Collectors.toList());
     }
 }
