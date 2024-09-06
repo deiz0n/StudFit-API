2b49bfd37c28735f35b8cfc80706983d9a08581c
deiz0n <eduardomacedosilva@outlook.com>
2024-09-06 18:53:37 -0300

    feat: Criação do AlunoRepository

---
+++ src/main/java/com/deiz0n/studfit/repositories/AlunoRepository.java
@@ -0,0 +1,9 @@
+package com.deiz0n.studfit.repositories;
+
+import com.deiz0n.studfit.domain.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
+}
