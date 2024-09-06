1a947c688d4c5c41efaec7a208321a67b67f1337
deiz0n <eduardomacedosilva@outlook.com>
2024-09-06 19:20:49 -0300

    feat: Implementação do padrão builder no AlunoDTO

--- src/main/java/com/deiz0n/studfit/domain/dtos/AlunoDTO.java
+++ src/main/java/com/deiz0n/studfit/domain/dtos/AlunoDTO.java
@@ -2,6 +2,7 @@ package com.deiz0n.studfit.domain.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +11,7 @@ import java.util.UUID;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class AlunoDTO {
 
     private UUID id;
