7bd5dbea7b6e6f7474b4cd57b17e3d4da2859f80
deiz0n <eduardomacedosilva@outlook.com>
2024-09-08 21:20:43 -0300

    feat: Criação do endpoint "/lista-espera" no AlunoController

--- src/main/java/com/deiz0n/studfit/controller/AlunoController.java
+++ src/main/java/com/deiz0n/studfit/controller/AlunoController.java
@@ -1,8 +1,15 @@
 package com.deiz0n.studfit.controller;
 
+import com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO;
 import com.deiz0n.studfit.services.AlunoService;
+import org.springframework.http.CacheControl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("api/v1.0/alunos")
@@ -12,5 +19,13 @@ public class AlunoController {
 
     public AlunoController(AlunoService service) {
         this.service = service;
+    }
+
+    @GetMapping("lista-espera")
+    public ResponseEntity<List<AlunoListaEsperaDTO>> getListaDeEspera() {
+        var alunos = service.getListaDeEspera();
+        return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
+                .body(alunos);
     }
 }
