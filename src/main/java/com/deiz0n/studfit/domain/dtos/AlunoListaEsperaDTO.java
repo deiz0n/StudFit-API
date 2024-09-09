432e316e71c4244f4bafb3fd937795ed719d9335
deiz0n <eduardomacedosilva@outlook.com>
2024-09-08 21:21:06 -0300

    fix: Ajustes ao retornar os alunos da lista de espera

--- src/main/java/com/deiz0n/studfit/domain/dtos/AlunoListaEsperaDTO.java
+++ src/main/java/com/deiz0n/studfit/domain/dtos/AlunoListaEsperaDTO.java
@@ -1,5 +1,6 @@
 package com.deiz0n.studfit.domain.dtos;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -17,6 +18,7 @@ public class AlunoListaEsperaDTO {
     private String nome;
     private String email;
     private Integer colocacao;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Boolean ativado = false;
 
 }
