cc227227ae3dea94389b02e232c619c4623cc243
deiz0n <eduardomacedosilva@outlook.com>
2024-09-09 14:53:24 -0300

    feat: Criação do método getEfetivados no AlunoService

--- src/main/java/com/deiz0n/studfit/services/AlunoService.java
+++ src/main/java/com/deiz0n/studfit/services/AlunoService.java
@@ -1,5 +1,6 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.AlunoDTO;
 import com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO;
 import com.deiz0n.studfit.repositories.AlunoRepository;
 import org.springframework.stereotype.Service;
@@ -25,10 +26,33 @@ public class AlunoService {
                             aluno.getNome(),
                             aluno.getEmail(),
                             aluno.getColocacao(),
-                            aluno.getAtivado()
+                            aluno.getListaEspera()
                         )
                 )
-                .filter(aluno -> !aluno.getAtivado())
+                .filter(AlunoListaEsperaDTO::getListaEspera)
+                .collect(Collectors.toList());
+    }
+
+    // Retorna todos os alunos já cadastrados na academia
+    public List<AlunoDTO> getEfetivados() {
+        return alunoRepository.findAll()
+                .stream()
+                .map(aluno -> new AlunoDTO(
+                        aluno.getId(),
+                        aluno.getNome(),
+                        aluno.getPeso(),
+                        aluno.getAltura(),
+                        aluno.getEmail(),
+                        aluno.getTelefone(),
+                        aluno.getCirurgias(),
+                        aluno.getPatologias(),
+                        aluno.getMesesExperienciaMusculacao(),
+                        aluno.getDiagnosticoLesaoJoelho(),
+                        aluno.getStatus(),
+                        aluno.getAusenciasConsecutivas(),
+                        aluno.getListaEspera()
+                ))
+                .filter(aluno -> !aluno.getListaEspera())
                 .collect(Collectors.toList());
     }
 }
