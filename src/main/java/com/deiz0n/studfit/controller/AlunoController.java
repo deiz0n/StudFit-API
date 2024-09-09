cf69eaef92885e1193badc1a9c231999a92d6f37
deiz0n <eduardomacedosilva@outlook.com>
2024-09-08 21:56:46 -0300

    refactor: Agora um endpoint "/lista-espera" retorna um ResponseRequest

--- src/main/java/com/deiz0n/studfit/controller/AlunoController.java
+++ src/main/java/com/deiz0n/studfit/controller/AlunoController.java
@@ -1,14 +1,16 @@
 package com.deiz0n.studfit.controller;
 
-import com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO;
+import com.deiz0n.studfit.domain.response.ResponseRequest;
 import com.deiz0n.studfit.services.AlunoService;
 import org.springframework.http.CacheControl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -22,10 +24,15 @@ public class AlunoController {
     }
 
     @GetMapping("lista-espera")
-    public ResponseEntity<List<AlunoListaEsperaDTO>> getListaDeEspera() {
+    public ResponseEntity<ResponseRequest> getListaDeEspera(ServletWebRequest request) {
         var alunos = service.getListaDeEspera();
         return ResponseEntity.ok()
                 .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(alunos);
+                .body(ResponseRequest.builder()
+                        .code(HttpStatus.OK.value())
+                        .status(HttpStatus.OK)
+                        .path(request.getRequest().getRequestURI())
+                        .data(alunos)
+                        .build());
     }
 }
