b57c684d451db4b3edea2fe571be7a7ef93d706a
deiz0n <eduardomacedosilva@outlook.com>
2024-09-10 14:37:26 -0300

    fix: Implementação da validação ao inserir um novo AlunoListaEsperaDTO

--- src/main/java/com/deiz0n/studfit/domain/dtos/AlunoListaEsperaDTO.java
+++ src/main/java/com/deiz0n/studfit/domain/dtos/AlunoListaEsperaDTO.java
@@ -1,6 +1,11 @@
 package com.deiz0n.studfit.domain.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +20,15 @@ import java.util.UUID;
 public class AlunoListaEsperaDTO {
 
     private UUID id;
+    @NotBlank(message = "O campo \"nome\" é obrigatório")
     private String nome;
+    @NotBlank(message = "O campo \"email\" é obrigatório")
+    @Email(message = "Email inválido. Tente novamente")
     private String email;
+    @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Integer colocacao;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY, namespace = "lista_espera")
-    private Boolean listaEspera = false;
+    @JsonIgnore
+    @JsonProperty(namespace = "lista_espera")
+    private Boolean listaEspera = true;
 
 }
