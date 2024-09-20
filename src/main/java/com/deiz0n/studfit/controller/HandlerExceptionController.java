b79bfd1af98c2b951cb2a3b081c6f705a6a00d3e
deiz0n <eduardomacedosilva@outlook.com>
2024-09-20 19:45:27 -0300

    feat: Criação do HandlerExceptionController

---
+++ src/main/java/com/deiz0n/studfit/controller/HandlerExceptionController.java
@@ -0,0 +1,10 @@
+package com.deiz0n.studfit.controller;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class HandlerExceptionController extends ResponseEntityExceptionHandler {
+
+
+}
