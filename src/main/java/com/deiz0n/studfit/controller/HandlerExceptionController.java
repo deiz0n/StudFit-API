edaff93a9decde7cbbad105097e56c2944fb7448
deiz0n <eduardomacedosilva@outlook.com>
2024-09-21 22:47:34 -0300

    feat: Captura de exceções do ResponseEntityExceptionHandler

--- src/main/java/com/deiz0n/studfit/controller/HandlerExceptionController.java
+++ src/main/java/com/deiz0n/studfit/controller/HandlerExceptionController.java
@@ -2,14 +2,47 @@ package com.deiz0n.studfit.controller;
 
 import com.deiz0n.studfit.domain.exceptions.ResourceNotFoundException;
 import com.deiz0n.studfit.domain.response.ResponseError;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @RestControllerAdvice
-public class HandlerExceptionController  {
+public class HandlerExceptionController extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ResponseError.builder()
+                                .code(HttpStatus.BAD_REQUEST.value())
+                                .title("Erro ao realizar a requesição")
+                                .status(HttpStatus.BAD_REQUEST)
+                                .description(ex.getFieldError().getDefaultMessage())
+                                .build()
+                );
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleNoResourceFoundException(NoResourceFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(
+                        ResponseError.builder()
+                                .code(HttpStatus.NOT_FOUND.value())
+                                .title("Recurso não encontrado")
+                                .status(HttpStatus.NOT_FOUND)
+                                .description("Rota inexistente")
+                                .build()
+                );
+    }
 
     @ExceptionHandler
     public ResponseEntity<ResponseError> handleResourceNotFoundException(ResourceNotFoundException exception) {
@@ -18,7 +51,7 @@ public class HandlerExceptionController  {
                 .body(
                         ResponseError.builder()
                                 .code(HttpStatus.NOT_FOUND.value())
-                                .title("Resource not found")
+                                .title("Recurso não encontrado")
                                 .status(HttpStatus.NOT_FOUND)
                                 .description(exception.getMessage())
                                 .build()
