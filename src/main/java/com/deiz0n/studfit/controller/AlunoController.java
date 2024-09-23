61f8074839d3fd64c2b3cb9d80bb84d9f755755c
deiz0n <eduardomacedosilva@outlook.com>
2024-09-23 15:56:04 -0300

    feat: Criação da interface AlunoController

--- src/main/java/com/deiz0n/studfit/controller/AlunoController.java
+++ src/main/java/com/deiz0n/studfit/controller/AlunoController.java
@@ -3,101 +3,33 @@ package com.deiz0n.studfit.controller;
 import com.deiz0n.studfit.domain.dtos.AlunoDTO;
 import com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO;
 import com.deiz0n.studfit.domain.response.ResponseRequest;
-import com.deiz0n.studfit.services.AlunoService;
 import jakarta.validation.Valid;
-import org.springframework.http.CacheControl;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
-@RestController
-@RequestMapping("api/v1.0/alunos")
-public class AlunoController {
-
-    private AlunoService service;
-
-    public AlunoController(AlunoService service) {
-        this.service = service;
-    }
+public interface AlunoController {
 
     @GetMapping("lista-espera")
-    public ResponseEntity<ResponseRequest> getAlunosListaEspera(ServletWebRequest path) {
-        var alunos = service.getListaDeEspera();
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(ResponseRequest.builder()
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK)
-                        .path(path.getRequest().getRequestURI())
-                        .data(alunos)
-                        .build());
-    }
+    ResponseEntity<ResponseRequest> getAlunosListaEspera(ServletWebRequest path);
 
     @PostMapping("/lista-espera/register")
-    public ResponseEntity<ResponseRequest> registerAlunoListaEspera(@RequestBody @Valid AlunoListaEsperaDTO request, ServletWebRequest path) {
-        var aluno = service.registerListaEspera(request);
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(ResponseRequest.builder()
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK)
-                        .path(path.getRequest().getRequestURI())
-                        .data(aluno)
-                        .build());
-    }
+    ResponseEntity<ResponseRequest> registerAlunoListaEspera(@RequestBody @Valid AlunoListaEsperaDTO request, ServletWebRequest path);
 
     @DeleteMapping("/lista-espera/delete/{id}")
-    public ResponseEntity removeAlunoListaEspera(@PathVariable UUID id, ServletWebRequest path) {
-        var aluno = service.removeListaEspera(id);
-        return ResponseEntity.noContent().build();
-    }
+    ResponseEntity removeAlunoListaEspera(@PathVariable UUID id, ServletWebRequest path);
 
     @GetMapping("efetivados")
-    public ResponseEntity<ResponseRequest> getAlunosEfetivados(ServletWebRequest path) {
-        var alunos = service.getEfetivados();
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(ResponseRequest.builder()
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK)
-                        .path(path.getRequest().getRequestURI())
-                        .data(alunos)
-                        .build());
-    }
+    ResponseEntity<ResponseRequest> getAlunosEfetivados(ServletWebRequest path);
 
     @PatchMapping("efetivados/efetivar")
-    public ResponseEntity<ResponseRequest> registerAlunoEfetivado(@RequestBody @Valid AlunoDTO request, ServletWebRequest path) {
-        var aluno = service.registerEfetivado(request);
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(ResponseRequest.builder()
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK)
-                        .path(path.getRequest().getRequestURI())
-                        .data(aluno)
-                        .build());
-    }
+    ResponseEntity<ResponseRequest> registerAlunoEfetivado(@RequestBody @Valid AlunoDTO request, ServletWebRequest path);
 
     @DeleteMapping("efetivados/delete/{id}")
-    public ResponseEntity removeAlunoEfetivado(@PathVariable UUID id) {
-        service.removeEfetivado(id);
-        return ResponseEntity.noContent().build();
-    }
+    ResponseEntity removeAlunoEfetivado(@PathVariable UUID id);
 
     @PatchMapping("efetivado/update/{id}")
-    public ResponseEntity<ResponseRequest> updateAlunoEfetivado(@PathVariable UUID id, @RequestBody @Valid AlunoDTO request, ServletWebRequest path) {
-        var aluno = service.updateEfetivado(id, request);
-        return ResponseEntity.ok()
-                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
-                .body(ResponseRequest.builder()
-                        .code(HttpStatus.OK.value())
-                        .status(HttpStatus.OK)
-                        .path(path.getRequest().getRequestURI())
-                        .data(aluno)
-                        .build());
-    }
+    ResponseEntity<ResponseRequest> updateAlunoEfetivado(@PathVariable UUID id, @RequestBody @Valid AlunoDTO request, ServletWebRequest path);
 }
