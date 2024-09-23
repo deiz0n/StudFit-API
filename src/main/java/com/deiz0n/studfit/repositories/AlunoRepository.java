603449633c37d1e3770db285a5fec0ec7311ca98
deiz0n <eduardomacedosilva@outlook.com>
2024-09-23 10:37:05 -0300

    feat: Criação da query para obter o primeiro aluno na lista de espera

--- src/main/java/com/deiz0n/studfit/repositories/AlunoRepository.java
+++ src/main/java/com/deiz0n/studfit/repositories/AlunoRepository.java
@@ -3,7 +3,11 @@ package com.deiz0n.studfit.repositories;
 import com.deiz0n.studfit.domain.entites.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
+
+    Optional<Aluno> getByColocacao(Integer colocacao);
+
 }
