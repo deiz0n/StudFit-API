0fe7f387981e5378e6e1e907eed83358cce52b73
deiz0n <eduardomacedosilva@outlook.com>
2024-09-26 16:21:41 -0300

    refactor: Criação do enum cargo responsável por identificar o cargo do usuário

---
+++ src/main/java/com/deiz0n/studfit/domain/enums/Cargo.java
@@ -0,0 +1,13 @@
+package com.deiz0n.studfit.domain.enums;
+
+public enum Cargo {
+
+    INSTRUTOR("instrutr"),
+    ESTAGIARIO("estagiario");
+
+    private String cargo;
+
+    Cargo(String cargo) {
+        this.cargo = cargo;
+    }
+}
