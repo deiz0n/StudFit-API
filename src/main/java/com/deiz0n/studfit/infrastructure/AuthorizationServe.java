bf51a80775a1e354cfe449964e01c5eb50f81a0e
deiz0n <eduardomacedosilva@outlook.com>
2024-09-26 16:30:49 -0300

    feat: Criação do AuthorizationServe

---
+++ src/main/java/com/deiz0n/studfit/infrastructure/AuthorizationServe.java
@@ -0,0 +1,22 @@
+package com.deiz0n.studfit.infrastructure;
+
+import com.deiz0n.studfit.repositories.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorizationServe implements UserDetailsService {
+
+    private UsuarioRepository repository;
+
+    public AuthorizationServe(UsuarioRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.getByEmail(username);
+    }
+}
