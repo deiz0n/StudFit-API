73871e1cb9566d29e27cceb2657991bf155ac8b8
deiz0n <eduardomacedosilva@outlook.com>
2024-09-27 10:42:20 -0300

    feat: Criação do TokenService

---
+++ src/main/java/com/deiz0n/studfit/infrastructure/security/TokenService.java
@@ -0,0 +1,14 @@
+package com.deiz0n.studfit.infrastructure.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenService {
+
+
+
+}
