47677bff3197efacc29aee24f022db23a5c8fc27
deiz0n <eduardomacedosilva@outlook.com>
2024-09-27 10:44:44 -0300

    feat: Criação do método generateToken no TokenService

--- src/main/java/com/deiz0n/studfit/infrastructure/security/TokenService.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/security/TokenService.java
@@ -6,9 +6,36 @@ import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
 @Service
 public class TokenService {
 
+    private static final String ISSUER = "stud-fit";
+    @Value("${api.secret.key.jwt}")
+    private String secret;
 
+    public void generateToken(String email) {
+        try {
+            var algorithm = Algorithm.HMAC256(secret);
+
+            var token = JWT.create()
+                    .withIssuer(ISSUER)
+                    .withSubject(email)
+                    .withExpiresAt(expirationInstant())
+                    .sign(algorithm);
+
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao criar Token", e);
+        }
+    }
+
+
+    public Instant expirationInstant() {
+        return LocalDateTime.now().plusHours(8).toInstant(ZoneOffset.of("03-00"));
+    }
 
 }
