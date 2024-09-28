fb90fa964893e97f80acd970f6b0dc51995e4b49
deiz0n <eduardomacedosilva@outlook.com>
2024-09-28 00:31:47 -0300

    feat: Criação do UsuarioDTO

---
+++ src/main/java/com/deiz0n/studfit/domain/dtos/UsuarioDTO.java
@@ -0,0 +1,26 @@
+package com.deiz0n.studfit.domain.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UsuarioDTO {
+
+    private UUID id;
+    @NotBlank(message = "O campo nome é  obrigatório")
+    private String nome;
+    @Email(message = "Email inválido. Tente novamente")
+    private String email;
+    @NotBlank(message = "O campo cargo é  obrigatório")
+    private String cargo;
+
+}
