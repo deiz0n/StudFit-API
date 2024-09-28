9b34b7795c65ffb3eee0ff3c5b720e2cc4c52011
deiz0n <eduardomacedosilva@outlook.com>
2024-09-28 20:30:25 -0300

    fix: Ajustes no BCryptPasswordEncoderConfig

--- src/main/java/com/deiz0n/studfit/infrastructure/config/BCryptPasswordEncoderConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/config/BCryptPasswordEncoderConfig.java
@@ -1,14 +1,15 @@
 package com.deiz0n.studfit.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BCryptPasswordEncoderConfig {
 
     @Bean
-    public BCryptPasswordEncoderConfig encoder() {
-        return new BCryptPasswordEncoderConfig();
+    public BCryptPasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
