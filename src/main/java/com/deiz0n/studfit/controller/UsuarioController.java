6e04af5fa3a750c6adcd2520793f3d597e5f8a5f
deiz0n <eduardomacedosilva@outlook.com>
2024-09-28 01:17:14 -0300

    feat: Criação do método getUsuarios no UsuarioController

--- src/main/java/com/deiz0n/studfit/controller/UsuarioController.java
+++ src/main/java/com/deiz0n/studfit/controller/UsuarioController.java
@@ -4,12 +4,15 @@ import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
 import com.deiz0n.studfit.domain.response.ResponseRequest;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.ServletWebRequest;
 
 public interface UsuarioController {
 
+    @GetMapping
+    public ResponseEntity<ResponseRequest> getUsuarios();
     @PostMapping
     public ResponseEntity<ResponseRequest> create(@RequestBody @Valid UsuarioDTO request, ServletWebRequest path);
 
