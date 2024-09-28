c69edcd0573c3188e6bbb009ee107d64d47946d7
deiz0n <eduardomacedosilva@outlook.com>
2024-09-28 00:43:15 -0300

    feat: Criação do UsuarioControllerImpl

---
+++ src/main/java/com/deiz0n/studfit/controller/UsuarioControllerImpl.java
@@ -0,0 +1,15 @@
+package com.deiz0n.studfit.controller;
+
+import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
+import com.deiz0n.studfit.domain.response.ResponseRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api;v1.0/usuarios")
+public class UsuarioControllerImpl {
+
+
+
+}
