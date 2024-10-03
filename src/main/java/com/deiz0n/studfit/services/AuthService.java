1b2f1cf1f277d166468f34d81e5a3bbaf2564b9f
deiz0n <eduardomacedosilva@outlook.com>
2024-10-03 09:24:31 -0300

    feat: Criação do TokenGenerationEvent

--- src/main/java/com/deiz0n/studfit/services/AuthService.java
+++ src/main/java/com/deiz0n/studfit/services/AuthService.java
@@ -1,7 +1,25 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.AuthDTO;
+import com.deiz0n.studfit.domain.dtos.TokenDTO;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
+
+    private TokenDTO token;
+    private AuthenticationManager manager;
+
+    public AuthService(AuthenticationManager manager) {
+        this.manager = manager;
+    }
+
+    public TokenDTO signIn(AuthDTO auth) {
+        var user = new UsernamePasswordAuthenticationToken(auth.getEmail(), auth.getSenha());
+        var authentication = manager.authenticate(user);
+
+        return token;
+    }
 }
