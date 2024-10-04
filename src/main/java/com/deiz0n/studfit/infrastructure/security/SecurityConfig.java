987066ce85f171b4f415e973291d7c255b2c7ca9
deiz0n <eduardomacedosilva@outlook.com>
2024-10-04 15:41:40 -0300

    fix: Ajustes no SecurityConfig

--- src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.deiz0n.studfit.infrastructure.security;
 
+import com.deiz0n.studfit.infrastructure.security.exceptions.CustomAccessDeniedHandler;
+import com.deiz0n.studfit.infrastructure.security.exceptions.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,10 +9,21 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private CustomAccessDeniedHandler accessDeniedHandler;
+    private CustomAuthenticationEntryPoint authenticationEntryPoint;
+    private SecurityFilter filter;
+
+    public SecurityConfig(CustomAccessDeniedHandler accessDeniedHandler, CustomAuthenticationEntryPoint authenticationEntryPoint, SecurityFilter filter) {
+        this.accessDeniedHandler = accessDeniedHandler;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.filter = filter;
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
@@ -30,6 +43,11 @@ public class SecurityConfig {
                         .requestMatchers(HttpMethod.PATCH, "api/v1.0/alunos/efetivados/update/{id}").hasRole("ESTAGIARIO")
 
                         .requestMatchers(HttpMethod.POST, "api/v1.0/auth/login").permitAll()
+                )
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+                .exceptionHandling(handlingConfigurer -> handlingConfigurer
+                        .accessDeniedHandler(accessDeniedHandler)
+                        .authenticationEntryPoint(authenticationEntryPoint)
                 )
                 .build();
     }
