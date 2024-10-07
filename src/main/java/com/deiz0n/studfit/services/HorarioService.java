83734a40b396b67fea2dfa7ac6f9f08ecbdcd82d
deiz0n <eduardomacedosilva@outlook.com>
2024-10-07 19:05:35 -0300

    feat: Criação do método getAll no HorarioService

--- src/main/java/com/deiz0n/studfit/services/HorarioService.java
+++ src/main/java/com/deiz0n/studfit/services/HorarioService.java
@@ -1,7 +1,28 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.HorarioDTO;
+import com.deiz0n.studfit.infrastructure.repositories.HorarioRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class HorarioService {
+
+    private HorarioRepository repository;
+    private ModelMapper mapper;
+
+    public HorarioService(HorarioRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public List<HorarioDTO> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(horario -> mapper.map(horario, HorarioDTO.class))
+                .collect(Collectors.toList());
+    }
 }
