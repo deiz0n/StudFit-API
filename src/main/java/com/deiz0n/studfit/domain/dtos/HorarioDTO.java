378ccf55def2b79438c593ce4401b64e5ef73034
deiz0n <eduardomacedosilva@outlook.com>
2024-10-07 18:36:56 -0300

    feat: Criação do HorarioDTO

---
+++ src/main/java/com/deiz0n/studfit/domain/dtos/HorarioDTO.java
@@ -0,0 +1,29 @@
+package com.deiz0n.studfit.domain.dtos;
+
+import com.deiz0n.studfit.domain.enums.Turno;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HorarioDTO {
+
+    private UUID id;
+    @JsonFormat(pattern = "HH:mm")
+    @JsonProperty("horario_inicial")
+    private LocalTime horarioInicial;
+    @JsonFormat(pattern = "HH:mm")
+    @JsonProperty("horario_inicial")
+    private LocalTime horarioFinal;
+    private Turno turno;
+
+}
