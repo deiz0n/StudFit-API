7bcbe5d0ecfe4c095486156cc02b958569f6de22
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 10:47:55 -0300

    refactor: O FrequenciaRepository foi alterado para PresencaRepository

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/PresencaRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/PresencaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface FrequenciaRepository extends JpaRepository<Presenca, UUID> {
+public interface PresencaRepository extends JpaRepository<Presenca, UUID> {
 }
