9e35e8d8d375da7ca3b2de077b84552a126e5665
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 09:03:33 -0300

    feat: Criação e tratamento do HorarioAlreadyRegistered

---
+++ src/main/java/com/deiz0n/studfit/domain/exceptions/HorarioAlreadyRegistered.java
@@ -0,0 +1,8 @@
+package com.deiz0n.studfit.domain.exceptions;
+
+public class HorarioAlreadyRegistered extends ResourceAlreadyException{
+
+    public HorarioAlreadyRegistered(String msg) {
+        super(msg);
+    }
+}
