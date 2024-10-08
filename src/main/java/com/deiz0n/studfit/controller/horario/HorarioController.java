d884c0f5df40e03534d778f7e98bb755fdb9d6bb
deiz0n <eduardomacedosilva@outlook.com>
2024-10-07 21:16:40 -0300

    feat: Criação do método createHorario no HorarioController

--- src/main/java/com/deiz0n/studfit/controller/horario/HorarioController.java
+++ src/main/java/com/deiz0n/studfit/controller/horario/HorarioController.java
@@ -1,13 +1,18 @@
 package com.deiz0n.studfit.controller.horario;
 
+import com.deiz0n.studfit.domain.dtos.HorarioDTO;
 import com.deiz0n.studfit.domain.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.context.request.ServletWebRequest;
 
 public interface HorarioController {
 
     @GetMapping
     ResponseEntity<Response> getHorarios(ServletWebRequest path);
+    @PostMapping("/create")
+    ResponseEntity<Response> createHorario(@RequestBody HorarioDTO request, ServletWebRequest path);
 
 }
