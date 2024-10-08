f77b0966d435695b355c39987146ba3c6fc9b12a
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 10:58:27 -0300

    feat: Criação do método getAll no PresencaService

--- src/main/java/com/deiz0n/studfit/domain/dtos/PresencaDTO.java
+++ src/main/java/com/deiz0n/studfit/domain/dtos/PresencaDTO.java
@@ -1,6 +1,7 @@
 package com.deiz0n.studfit.domain.dtos;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,6 +20,7 @@ public class PresencaDTO {
     @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate data;
     private Boolean presente;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private AlunoDTO aluno;
     private UsuarioDTO usuario;
 }
--- src/main/java/com/deiz0n/studfit/services/PresencaService.java
+++ src/main/java/com/deiz0n/studfit/services/PresencaService.java
@@ -1,10 +1,28 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.PresencaDTO;
+import com.deiz0n.studfit.infrastructure.repositories.PresencaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PresencaService {
 
+    private PresencaRepository repository;
+    private ModelMapper mapper;
 
+    public PresencaService(PresencaRepository repository, ModelMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
 
+    public List<PresencaDTO> getAll() {
+        return repository.findAll()
+                .stream()
+                .map(presenca -> mapper.map(presenca, PresencaDTO.class))
+                .collect(Collectors.toList());
+    }
 }
