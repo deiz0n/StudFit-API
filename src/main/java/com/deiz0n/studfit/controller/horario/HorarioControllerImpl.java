59b3cf1ae0ce40570b66434745440208b3cd4683
deiz0n <eduardomacedosilva@outlook.com>
2024-10-07 21:17:29 -0300

    feat: Criação do endpoint /create no HorarioController

--- src/main/java/com/deiz0n/studfit/controller/horario/HorarioControllerImpl.java
+++ src/main/java/com/deiz0n/studfit/controller/horario/HorarioControllerImpl.java
@@ -1,5 +1,6 @@
 package com.deiz0n.studfit.controller.horario;
 
+import com.deiz0n.studfit.domain.dtos.HorarioDTO;
 import com.deiz0n.studfit.domain.response.Response;
 import com.deiz0n.studfit.services.HorarioService;
 import org.springframework.http.CacheControl;
@@ -8,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.ServletWebRequest;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,11 +27,30 @@ public class HorarioControllerImpl  implements HorarioController{
     public ResponseEntity<Response> getHorarios(ServletWebRequest path) {
         var horarios = service.getAll();
         return ResponseEntity.ok()
+                .cacheControl(CacheControl.maxAge(3, TimeUnit.MINUTES))
                 .body(Response.builder()
                         .code(HttpStatus.OK.value())
                         .status(HttpStatus.OK)
                         .path(path.getRequest().getRequestURI())
                         .data(horarios)
                         .build());
+    }
+
+    @Override
+    public ResponseEntity<Response> createHorario(HorarioDTO request, ServletWebRequest path) {
+        var horario = service.create(request);
+        var uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("id")
+                .buildAndExpand(horario.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri)
+                .body(Response.builder()
+                .code(HttpStatus.CREATED.value())
+                .status(HttpStatus.CREATED)
+                .path(path.getRequest().getRequestURI())
+                .data(horario)
+                .build());
     }
 }
