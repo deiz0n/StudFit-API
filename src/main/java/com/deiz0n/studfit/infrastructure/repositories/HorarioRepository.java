17a9c121e1f609c2c3a32cb8a31ef4014954a139
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 08:44:55 -0300

    feat: Criação da query getHorario no HorarioRepository

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
@@ -2,8 +2,15 @@ package com.deiz0n.studfit.infrastructure.repositories;
 
 import com.deiz0n.studfit.domain.entites.Horario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalTime;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface HorarioRepository extends JpaRepository<Horario, UUID> {
+
+    @Query("FROM Horario h WHERE h.horarioInicial = :start AND h.horarioFinal = :end")
+    Optional<Horario> getHorario(LocalTime start, LocalTime end);
+
 }
