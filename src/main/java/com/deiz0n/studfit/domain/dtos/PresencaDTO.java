4aef8b285b7e742396bdd92ebe879ca4bce64b25
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 10:36:25 -0300

    feat: Criação do PresencaDTO

---
+++ src/main/java/com/deiz0n/studfit/domain/dtos/PresencaDTO.java
@@ -0,0 +1,24 @@
+package com.deiz0n.studfit.domain.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class PresencaDTO {
+
+    private UUID id;
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate data;
+    private Boolean presente;
+    private AlunoDTO aluno;
+    private UsuarioDTO usuario;
+}
