ca8befa28af57fc7c1187bb9fe4dc5995198c4a8
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 11:20:29 -0300

    feat: Criação do método create no PresencaService

--- src/main/java/com/deiz0n/studfit/services/PresencaService.java
+++ src/main/java/com/deiz0n/studfit/services/PresencaService.java
@@ -1,10 +1,14 @@
 package com.deiz0n.studfit.services;
 
+import com.deiz0n.studfit.domain.dtos.AlunoDTO;
 import com.deiz0n.studfit.domain.dtos.PresencaDTO;
+import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
+import com.deiz0n.studfit.domain.entites.Presenca;
 import com.deiz0n.studfit.infrastructure.repositories.PresencaRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -24,5 +28,20 @@ public class PresencaService {
                 .stream()
                 .map(presenca -> mapper.map(presenca, PresencaDTO.class))
                 .collect(Collectors.toList());
+    }
+
+    public PresencaDTO create(PresencaDTO presencaDTO) {
+        var presenca = mapper.map(presencaDTO, Presenca.class);
+
+        presenca.setData(LocalDate.now());
+        repository.save(presenca);
+
+        return PresencaDTO.builder()
+                .id(presenca.getId())
+                .data(presenca.getData())
+                .presente(presenca.getPresente())
+                .aluno(mapper.map(presenca.getAluno(), AlunoDTO.class))
+                .usuario(mapper.map(presenca.getUsuario(), UsuarioDTO.class))
+                .build();
     }
 }
