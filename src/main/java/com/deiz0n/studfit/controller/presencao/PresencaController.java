0d6fe94683cdbbbe853b4745ea088881765bc017
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 11:02:07 -0300

    feat: Criação do endpoint "/presencas" no PresencaController

--- src/main/java/com/deiz0n/studfit/controller/presencao/PresencaController.java
+++ src/main/java/com/deiz0n/studfit/controller/presencao/PresencaController.java
@@ -3,10 +3,11 @@ package com.deiz0n.studfit.controller.presencao;
 import com.deiz0n.studfit.domain.response.Response;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.ServletWebRequest;
 
 public interface PresencaController {
 
     @GetMapping
-    ResponseEntity<Response> getAll();
+    ResponseEntity<Response> getAll(ServletWebRequest path);
 
 }
--- src/main/java/com/deiz0n/studfit/controller/presencao/PresencaControllerImpl.java
+++ src/main/java/com/deiz0n/studfit/controller/presencao/PresencaControllerImpl.java
@@ -1,11 +1,32 @@
 package com.deiz0n.studfit.controller.presencao;
 
+import com.deiz0n.studfit.domain.response.Response;
+import com.deiz0n.studfit.services.PresencaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.ServletWebRequest;
 
 @RestController
 @RequestMapping("api/v1.0/presencas")
-public class PresencaControllerImpl {
+public class PresencaControllerImpl implements PresencaController {
 
+    private PresencaService service;
 
+    public PresencaControllerImpl(PresencaService service) {
+        this.service = service;
+    }
+
+    @Override
+    public ResponseEntity<Response> getAll(ServletWebRequest path) {
+        var presencas = service.getAll();
+        return ResponseEntity.ok()
+                .body(Response.builder()
+                .code(HttpStatus.OK.value())
+                .status(HttpStatus.OK)
+                .path(path.getRequest().getRequestURI())
+                .data(presencas)
+                .build());
+    }
 }
