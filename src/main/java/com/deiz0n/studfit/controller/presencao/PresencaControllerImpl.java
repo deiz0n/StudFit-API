cb12fcba63c402e378ea2826e0be46ebf112d89d
deiz0n <eduardomacedosilva@outlook.com>
2024-10-08 10:59:16 -0300

    feat: Implementação do PresencaController

---
+++ src/main/java/com/deiz0n/studfit/controller/presencao/PresencaControllerImpl.java
@@ -0,0 +1,11 @@
+package com.deiz0n.studfit.controller.presencao;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1.0/presencas")
+public class PresencaControllerImpl {
+
+
+}
