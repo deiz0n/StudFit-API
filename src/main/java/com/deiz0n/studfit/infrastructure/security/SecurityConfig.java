0da217c21914b04688845d9ed8ea50e64a0de337
deiz0n <eduardomacedosilva@outlook.com>
2024-10-14 16:06:48 -0300

    fix: validação ao registrar uma presença

--- src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static com.deiz0n.studfit.infrastructure.config.CorsConfig.getCorsConfigurationSource;
 
@@ -13,12 +14,19 @@ import static com.deiz0n.studfit.infrastructure.config.CorsConfig.getCorsConfigu
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private SecurityFilter filter;
+
+    public SecurityConfig(SecurityFilter filter) {
+        this.filter = filter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(getCorsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
