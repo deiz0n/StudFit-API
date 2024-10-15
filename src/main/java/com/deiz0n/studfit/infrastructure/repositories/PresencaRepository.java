f76a122fe58c750d86b80e4e7cf569a0fb9463ba
deiz0n <eduardomacedosilva@outlook.com>
2024-10-15 18:42:49 -0300

    feat: criação da query getPresencas no PresencaRepository

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/PresencaRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/PresencaRepository.java
@@ -2,8 +2,14 @@ package com.deiz0n.studfit.infrastructure.repositories;
 
 import com.deiz0n.studfit.domain.entites.Presenca;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface PresencaRepository extends JpaRepository<Presenca, UUID> {
+
+    @Query("SELECT p FROM Presenca p WHERE p.aluno.id = :id")
+    List<Presenca> getPresencas(UUID id);
+
 }
