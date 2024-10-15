230edbe40efc2031a9098ad66d4d8abb773d47da
deiz0n <eduardomacedosilva@outlook.com>
2024-10-15 19:41:30 -0300

    feat: adição do cargo "ADMINISTRADOR" e definição de acesso as rotas

--- src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.deiz0n.studfit.infrastructure.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,6 +27,16 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable())
                 .cors(corsConfigurer -> corsConfigurer.configurationSource(getCorsConfigurationSource()))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers( "api/v1.0/alunos/efetivados", "api/v1.0/alunos/lista-espera", "api/v1.0/auth/login").permitAll()
+
+                        .requestMatchers("api/v1.0/horarios**").hasRole("INSTRUTOR")
+
+                        .requestMatchers("api/v1.0/alunos**", "api/v1.0/presencas**").hasRole("ESTAGIARIO")
+
+                        .requestMatchers("api/v1.0/usuarios**").hasRole("ADMINISTRADOR")
+                        .anyRequest().authenticated()
+                )
                 .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
