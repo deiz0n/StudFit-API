4e1d94f2b474507c0af5e2ec784ed68f16c42d0d
deiz0n <eduardomacedosilva@outlook.com>
2024-10-22 13:49:38 -0300

    feat: criação do PresencaNotValidException

---
+++ src/main/java/com/deiz0n/studfit/domain/exceptions/PresencaAlreadyRegistered.java
@@ -0,0 +1,9 @@
+package com.deiz0n.studfit.domain.exceptions;
+
+public class PresencaAlreadyRegistered extends ResourceAlreadyException {
+
+    public PresencaAlreadyRegistered(String msg) {
+        super(msg);
+    }
+
+}
