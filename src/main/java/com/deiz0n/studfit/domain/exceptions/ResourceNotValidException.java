20364eccaf6785a9bd780ee3c35ae9ba57681961
deiz0n <eduardomacedosilva@outlook.com>
2024-10-22 13:49:06 -0300

    feat: criação do ResourceNotValidException

---
+++ src/main/java/com/deiz0n/studfit/domain/exceptions/ResourceNotValidException.java
@@ -0,0 +1,9 @@
+package com.deiz0n.studfit.domain.exceptions;
+
+public class ResourceNotValidException extends RuntimeException{
+
+    public ResourceNotValidException(String msg) {
+        super(msg);
+    }
+
+}
