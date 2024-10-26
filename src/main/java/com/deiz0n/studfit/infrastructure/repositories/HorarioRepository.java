7e28164f3cea5720a9cb026edd516d9b6af97c06
deiz0n <eduardomacedosilva@outlook.com>
2024-10-26 18:40:30 -0300

    feat: criação da query getByTurno no HorarioRepository

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
@@ -1,10 +1,12 @@
 package com.deiz0n.studfit.infrastructure.repositories;
 
 import com.deiz0n.studfit.domain.entites.Horario;
+import com.deiz0n.studfit.domain.enums.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.time.LocalTime;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -12,5 +14,6 @@ public interface HorarioRepository extends JpaRepository<Horario, UUID> {
 
     @Query("FROM Horario h WHERE h.horarioInicial = :start AND h.horarioFinal = :end")
     Optional<Horario> getHorario(LocalTime start, LocalTime end);
+    List<Horario> getByTurno(Turno turno);
 
 }
