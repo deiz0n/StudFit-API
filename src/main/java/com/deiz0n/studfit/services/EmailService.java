dcdab304e5510a8bc711e7ba519ffb7c6badd109
deiz0n <eduardomacedosilva@outlook.com>
2024-10-28 23:21:06 -0300

    feat: criaçãp do método processTemplate

--- src/main/java/com/deiz0n/studfit/services/EmailService.java
+++ src/main/java/com/deiz0n/studfit/services/EmailService.java
@@ -1,12 +1,17 @@
 package com.deiz0n.studfit.services;
 
 import com.deiz0n.studfit.domain.dtos.EmailDTO;
+import com.deiz0n.studfit.domain.events.EmailRecoveryPasswordEvent;
 import com.deiz0n.studfit.domain.events.UsuarioRecoveryPassswordEvent;
+import com.deiz0n.studfit.infrastructure.config.AlgorithmGenerateNumber;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.event.EventListener;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 @Service
 public class EmailService {
@@ -14,33 +19,47 @@ public class EmailService {
     @Value("${spring.mail.username}")
     private String emailFrom;
     private JavaMailSender mailSender;
+    private Configuration freeMakerConfig;
 
-    public EmailService(JavaMailSender mailSender) {
+    public EmailService(JavaMailSender mailSender, Configuration freeMakerConfig) {
         this.mailSender = mailSender;
+        this.freeMakerConfig = freeMakerConfig;
     }
 
     private void sendMail(EmailDTO message) {
+        var content = processTemplate(message);
         var mimeMessage = mailSender.createMimeMessage();
         var helper = new MimeMessageHelper(mimeMessage, "UTF-8");
 
         try {
             helper.setTo(message.getDestinatario());
             helper.setSubject(message.getTitulo());
-            helper.setText(message.getConteudo(), true);
+            helper.setText(content, true);
             helper.setFrom(emailFrom);
 
             mailSender.send(mimeMessage);
         } catch (Exception e) {
-            throw new RuntimeException("Erro ao enviar o enail", e);
+            throw new RuntimeException("Erro ao enviar o email", e);
+        }
+    }
+
+    private String processTemplate(EmailDTO message) {
+        try {
+            Template template = freeMakerConfig.getTemplate(message.getConteudo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template, message.getVariaveis());
+        } catch (Exception e) {
+            throw new RuntimeException("Erro ao renderizar template do email", e);
         }
     }
 
     @EventListener
-    private void sendRecoveryPassword(UsuarioRecoveryPassswordEvent recoveryPassswordEvent) {
+    private void sendRecoveryPassword(EmailRecoveryPasswordEvent emailRecoveryPasswordEvent) {
         var email = EmailDTO.builder()
-                .destinatario(recoveryPassswordEvent.getEmail())
+                .destinatario(emailRecoveryPasswordEvent.getDestinatario())
                 .titulo("Solicitação para alteração de senha")
-                .conteudo("Seu código é: {{CÓDIGO}}")
+                .variavel("codigo", emailRecoveryPasswordEvent.getCodigo())
+                .conteudo("recovery-password.html")
                 .build();
 
         sendMail(email);
