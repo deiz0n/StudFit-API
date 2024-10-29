868891b4e2e49d0324aa9d508951d6c702bd9855
deiz0n <eduardomacedosilva@outlook.com>
2024-10-28 21:13:37 -0300

    feat: criação do EmailDTO

---
+++ src/main/java/com/deiz0n/studfit/domain/dtos/EmailDTO.java
@@ -0,0 +1,22 @@
+package com.deiz0n.studfit.domain.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDTO {
+
+    @Email(message = "Email inválido. Tente novamente")
+    @NotBlank(message = "O campo email é obrigatório")
+    private String destinatario;
+    private String titulo;
+    private String conteudo;
+
+}
