22425b9dd7d6f9041f8d993b42ccc9d84e927be0
deiz0n <eduardomacedosilva@outlook.com>
2024-10-28 21:14:44 -0300

    feat: criação do método recovery no AuthService

--- src/main/java/com/deiz0n/studfit/services/AuthService.java
+++ src/main/java/com/deiz0n/studfit/services/AuthService.java
@@ -1,11 +1,15 @@
 package com.deiz0n.studfit.services;
 
 import com.deiz0n.studfit.domain.dtos.AuthDTO;
+import com.deiz0n.studfit.domain.dtos.RecoveryPasswordDTO;
 import com.deiz0n.studfit.domain.dtos.TokenDTO;
 import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
 import com.deiz0n.studfit.domain.entites.Usuario;
 import com.deiz0n.studfit.domain.events.TokenGeneratedEvent;
 import com.deiz0n.studfit.domain.events.TokenGenerationEvent;
+import com.deiz0n.studfit.domain.events.UsuarioRecoveryPassswordEvent;
+import com.deiz0n.studfit.domain.exceptions.usuario.UsuarioNotFoundException;
+import com.deiz0n.studfit.infrastructure.repositories.UsuarioRepository;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,10 +22,12 @@ public class AuthService {
     private TokenDTO token;
     private AuthenticationManager manager;
     private ApplicationEventPublisher eventPublisher;
+    private UsuarioRepository usuarioRepository;
 
-    public AuthService(AuthenticationManager manager, ApplicationEventPublisher eventPublisher) {
+    public AuthService(AuthenticationManager manager, ApplicationEventPublisher eventPublisher, UsuarioRepository usuarioRepository) {
         this.manager = manager;
         this.eventPublisher = eventPublisher;
+        this.usuarioRepository = usuarioRepository;
     }
 
     public TokenDTO signIn(AuthDTO auth) {
@@ -32,6 +38,15 @@ public class AuthService {
         eventPublisher.publishEvent(tokenGeneration);
 
         return token;
+    }
+
+    public void recovery(RecoveryPasswordDTO recoveryPassword) {
+        var usuario = usuarioRepository.findByEmail(recoveryPassword.getEmail());
+        if (usuario.isEmpty())
+            throw new UsuarioNotFoundException("Usuário não encontrado");
+
+        var recoveryPasswordEvent = new UsuarioRecoveryPassswordEvent(this, recoveryPassword.getEmail());
+        eventPublisher.publishEvent(recoveryPasswordEvent);
     }
 
     @EventListener
