95e008b86c65e7b29d81e62fb424fab428845d37
deiz0n <eduardomacedosilva@outlook.com>
2024-11-06 11:26:00 -0300

    refactor: alterações no eventos para o envio de email

--- src/main/java/com/deiz0n/studfit/domain/events/SentEmailUpdatedPasswordEvent.java
+++ src/main/java/com/deiz0n/studfit/domain/events/SentEmailUpdatedPasswordEvent.java
@@ -6,9 +6,9 @@ import org.springframework.context.ApplicationEvent;
 @Getter
 public class SentEmailUpdatedPasswordEvent extends ApplicationEvent {
 
-    private String destinatario;
+    private final String[] destinatario;
 
-    public SentEmailUpdatedPasswordEvent(Object source, String destinatario) {
+    public SentEmailUpdatedPasswordEvent(Object source, String[] destinatario) {
         super(source);
         this.destinatario = destinatario;
     }
