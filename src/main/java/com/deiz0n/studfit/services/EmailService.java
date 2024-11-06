9101d3d943bbefb7c59a9fe89c5f5761adcfcc7a
deiz0n <eduardomacedosilva@outlook.com>
2024-11-06 11:28:49 -0300

    feat: implementação do evento de emails

--- src/main/java/com/deiz0n/studfit/services/EmailService.java
+++ src/main/java/com/deiz0n/studfit/services/EmailService.java
@@ -1,6 +1,8 @@
 package com.deiz0n.studfit.services;
 
 import com.deiz0n.studfit.domain.dtos.EmailDTO;
+import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
+import com.deiz0n.studfit.domain.events.SentAlunoDeletedToUsuariosEvent;
 import com.deiz0n.studfit.domain.events.SentEmailDeletedAlunoEfetivadoEvent;
 import com.deiz0n.studfit.domain.events.SentEmailRecoveryPasswordEvent;
 import com.deiz0n.studfit.domain.events.SentEmailUpdatedPasswordEvent;
@@ -13,6 +15,8 @@ import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
+
+import java.util.Arrays;
 
 @Service
 public class EmailService {
@@ -40,7 +44,7 @@ public class EmailService {
 
             mailSender.send(mimeMessage);
         } catch (Exception e) {
-            throw new SendEmailException("Erro ao enviar o email");
+            throw new RuntimeException("Erro ao enviar o email", e);
         }
     }
 
@@ -81,9 +85,26 @@ public class EmailService {
     private void sendDeletedAlunoEfetivado(SentEmailDeletedAlunoEfetivadoEvent deletedAlunoEfetivado) {
         var email = EmailDTO.builder()
                 .destinatario(deletedAlunoEfetivado.getDestinatario())
-                .titulo("Remoção por excesso de faltas")
+                .titulo("Exclusão de cadastro")
                 .variavel("nome_aluno", deletedAlunoEfetivado.getNome())
                 .conteudo("removed-by-absences.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendAlunoDeletedToUsuarios(SentAlunoDeletedToUsuariosEvent deletedToUsuarios) {
+        var listOfEmails = deletedToUsuarios.getUsuarios()
+                .stream()
+                .map(UsuarioDTO::getEmail)
+                .toArray(String[]::new);
+
+        var email = EmailDTO.builder()
+                .destinatario(listOfEmails)
+                .titulo("Exclusão de cadastro")
+                .variavel("nome_aluno", deletedToUsuarios.getNomeAluno())
+                .conteudo("aluno-removed.html")
                 .build();
 
         sendMail(email);
