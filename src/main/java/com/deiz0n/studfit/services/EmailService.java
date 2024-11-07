a2dd31fb593364c9f577e2d82fb3205766a5a166
deiz0n <eduardomacedosilva@outlook.com>
2024-11-07 18:46:23 -0300

    fix: correções de erros gerados pelo merge

--- src/main/java/com/deiz0n/studfit/services/EmailService.java
+++ src/main/java/com/deiz0n/studfit/services/EmailService.java
@@ -1,7 +1,8 @@
 package com.deiz0n.studfit.services;
 
 import com.deiz0n.studfit.domain.dtos.EmailDTO;
-import com.deiz0n.studfit.domain.events.EmailRecoveryPasswordEvent;
+import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
+import com.deiz0n.studfit.domain.events.*;
 import com.deiz0n.studfit.domain.exceptions.usuario.SendEmailException;
 import freemarker.template.Configuration;
 import freemarker.template.Template;
@@ -53,12 +54,76 @@ public class EmailService {
     }
 
     @EventListener
-    private void sendRecoveryPassword(EmailRecoveryPasswordEvent emailRecoveryPasswordEvent) {
+    private void sendRecoveryPassword(SentEmailRecoveryPasswordEvent emailRecoveryPasswordEvent) {
         var email = EmailDTO.builder()
                 .destinatario(emailRecoveryPasswordEvent.getDestinatario())
                 .titulo("Solicitação para alteração de senha")
                 .variavel("codigo", emailRecoveryPasswordEvent.getCodigo())
                 .conteudo("recovery-password.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendConfirmUpdatedPassword(SentEmailUpdatedPasswordEvent updatedPassword) {
+        var email = EmailDTO.builder()
+                .destinatario(updatedPassword.getDestinatario())
+                .titulo("Sua senha foi alterada com sucesso")
+                .conteudo("updated-password-success.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendDeletedAlunoEfetivado(SentEmailDeletedAlunoEfetivadoEvent deletedAlunoEfetivado) {
+        var email = EmailDTO.builder()
+                .destinatario(deletedAlunoEfetivado.getDestinatario())
+                .titulo("Exclusão de cadastro")
+                .variavel("nome_aluno", deletedAlunoEfetivado.getNome())
+                .conteudo("removed-by-absences.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendAlunoDeletedToUsuarios(SentAlunoDeletedToUsuariosEvent deletedToUsuarios) {
+        var listOfEmails = deletedToUsuarios.getUsuarios()
+                .stream()
+                .map(UsuarioDTO::getEmail)
+                .toArray(String[]::new);
+
+        var email = EmailDTO.builder()
+                .destinatario(listOfEmails)
+                .titulo("Exclusão de cadastro")
+                .variavel("nome_aluno", deletedToUsuarios.getNomeAluno())
+                .conteudo("aluno-removed.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendAlunoEfetivado(SentEmailAlunoEfetivadoEvent alunoEfetivado) {
+        var email = EmailDTO.builder()
+                .destinatario(alunoEfetivado.getDestinatario())
+                .titulo("Efetivação de cadastro")
+                .variavel("nome_aluno", alunoEfetivado.getNomeAluno())
+                .conteudo("aluno-efetivado.html")
+                .build();
+
+        sendMail(email);
+    }
+
+    @EventListener
+    private void sendAlunoEfetivadoToUsuarios(SentAlunoEfetivadoToUsuarios alunoEfetivadoToUsuarios) {
+        var email = EmailDTO.builder()
+                .destinatario(alunoEfetivadoToUsuarios.getDestinatario())
+                .titulo("Efetivação de cadastro")
+                .variavel("nome_aluno", alunoEfetivadoToUsuarios.getNomeAluno())
+                .conteudo("aluno-efetivado-to-usuarios.html")
                 .build();
 
         sendMail(email);
