8aa8bb14cd67d73bc4277e87938e69f4736995f6
deiz0n <eduardomacedosilva@outlook.com>
2025-01-11 18:57:48 -0300

    feat: adição da tag do swagger no UsuarioController

--- src/main/java/com/deiz0n/studfit/controller/usuario/UsuarioController.java
+++ src/main/java/com/deiz0n/studfit/controller/usuario/UsuarioController.java
@@ -2,6 +2,7 @@ package com.deiz0n.studfit.controller.usuario;
 
 import com.deiz0n.studfit.domain.dtos.UsuarioDTO;
 import com.deiz0n.studfit.domain.response.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -10,6 +11,7 @@ import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.UUID;
 
+@Tag(name = "Controller do Usuário")
 public interface UsuarioController {
 
     @GetMapping
