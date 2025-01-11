fed7c56e432c5d6be75c52df3b78c697e5c9ebd4
deiz0n <eduardomacedosilva@outlook.com>
2025-01-11 18:50:28 -0300

    fix: correções nas urls do PresencaController

--- src/main/java/com/deiz0n/studfit/controller/presenca/PresencaController.java
+++ src/main/java/com/deiz0n/studfit/controller/presenca/PresencaController.java
@@ -3,7 +3,9 @@ package com.deiz0n.studfit.controller.presenca;
 import com.deiz0n.studfit.domain.dtos.PresencaDTO;
 import com.deiz0n.studfit.domain.response.Response;
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,13 +15,17 @@ import org.springframework.web.context.request.ServletWebRequest;
 import java.time.LocalDate;
 import java.util.List;
 
+@Tag(name = "Controller da Presença")
 public interface PresencaController {
 
     @GetMapping
     ResponseEntity<Response> getPresencas(ServletWebRequest path);
-    @PostMapping("/create")
+
+    @Transactional
+    @PostMapping("/registrar")
     ResponseEntity<Response> createPresenca(@RequestBody List<PresencaDTO> request, ServletWebRequest path, @RequestParam @JsonFormat(pattern = "dd/MM/yyyy") LocalDate data);
-    @GetMapping("/")
+
+    @GetMapping("/buscar-por-data")
     ResponseEntity<Response> getPresencasByData(ServletWebRequest path, @RequestParam @JsonFormat(pattern = "dd/MM/yyyy") LocalDate data);
 
 }
