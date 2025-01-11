7204845e3124ebd7f758a8fd80b2792857984971
deiz0n <eduardomacedosilva@outlook.com>
2025-01-11 18:27:51 -0300

    refactor: ajustes nas urls do AuthController

--- src/main/java/com/deiz0n/studfit/controller/auth/AuthController.java
+++ src/main/java/com/deiz0n/studfit/controller/auth/AuthController.java
@@ -5,6 +5,7 @@ import com.deiz0n.studfit.domain.dtos.RecoveryPasswordDTO;
 import com.deiz0n.studfit.domain.dtos.ResetPasswordDTO;
 import com.deiz0n.studfit.domain.dtos.TokenDTO;
 import com.deiz0n.studfit.domain.response.Response;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,15 +13,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.ServletWebRequest;
 
+@Tag(name = "Controller de Autenticação")
 public interface AuthController {
 
     @PostMapping("/login")
     ResponseEntity<Response> singIn(@RequestBody @Valid AuthDTO request, ServletWebRequest path);
-    @PostMapping("/recovery-password")
+
+    @PostMapping("/recuperar-senha")
     ResponseEntity<Response> recoveryPassword(@RequestBody @Valid RecoveryPasswordDTO request, ServletWebRequest path);
-    @PostMapping("/recovery-password/")
+
+    @PostMapping("/recuperar-senha/")
     ResponseEntity<Response> setPassword(@RequestParam String codigo, @RequestBody @Valid ResetPasswordDTO request, ServletWebRequest path);
-    @PostMapping("/validate-token")
+
+    @PostMapping("/validar-token")
     ResponseEntity<Response> validateToken(@RequestBody TokenDTO request, ServletWebRequest path);
 
 }
