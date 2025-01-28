7b57ab79fe335134e48474a93d615da9cde2bc20
deiz0n <eduardomacedosilva@outlook.com>
2025-01-28 19:12:33 -0300

    refactor: ajustes no HorarioDTO

--- src/main/java/com/deiz0n/studfit/domain/dtos/HorarioDTO.java
+++ src/main/java/com/deiz0n/studfit/domain/dtos/HorarioDTO.java
@@ -4,15 +4,13 @@ import com.deiz0n.studfit.domain.enums.Turno;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
-import java.util.Comparator;
 import java.util.UUID;
 
 @Data
@@ -24,15 +22,18 @@ public class HorarioDTO implements Comparable<HorarioDTO> {
     private UUID id;
     @JsonFormat(pattern = "HH:mm")
     @JsonProperty("horario_inicial")
+    @NotBlank(message = "O campo horario_inicial é obrigatório")
     private LocalTime horarioInicial;
     @JsonFormat(pattern = "HH:mm")
     @JsonProperty("horario_final")
+    @NotBlank(message = "O campo horario_inicial é obrigatório")
     private LocalTime horarioFinal;
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private String turno;
+    private Turno turno;
     @JsonInclude(JsonInclude.Include.NON_NULL)
     @JsonProperty(value = "vagas_disponiveis", access = JsonProperty.Access.READ_ONLY)
     private Integer vagasDisponiveis;
+    private HorarioDTO horarioDTO;
 
     @Override
     public int compareTo(HorarioDTO horario) {
