fe422d81f5accbbbd50aa0212140c611bd0082ea
deiz0n <eduardomacedosilva@outlook.com>
2025-02-08 15:23:17 -0300

    fix: ajustes na query que retorna os alunos efetivados

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/AlunoRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/AlunoRepository.java
@@ -1,5 +1,6 @@
 package com.deiz0n.studfit.infrastructure.repositories;
 
+import com.deiz0n.studfit.domain.dtos.AlunoEfetivadoDTO;
 import com.deiz0n.studfit.domain.entites.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -12,10 +13,18 @@ public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
 
     @Query("FROM tb_aluno a WHERE a.colocacao = :colocacao")
     Optional<Aluno> buscarPorColocacao(Integer colocacao);
+
     @Query("FROM tb_aluno a WHERE a.email = :email")
     Optional<Aluno> buscarPorEmail(String email);
+
     @Query("FROM tb_aluno a WHERE a.telefone = :telefone")
     Optional<Aluno> buscarPorTelefone(String telefone);
+
     @Query("FROM tb_aluno a WHERE a.listaEspera = true")
     List<Aluno> buscarAlunosListaEspera();
+
+    @Query("SELECT NEW " +
+            "com.deiz0n.studfit.domain.dtos.AlunoEfetivadoDTO(a.id, a.nome, NEW com.deiz0n.studfit.domain.dtos.HorarioDTO(h.horarioInicial, h.horarioFinal, h.turno))" +
+            " FROM tb_aluno a JOIN FETCH tb_horario h ON a.horario.id = h.id")
+    List<AlunoEfetivadoDTO> buscarAlunosEfetivados();
 }
