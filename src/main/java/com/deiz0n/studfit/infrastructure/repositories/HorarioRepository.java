9a8408368ecca159438a133714ce7b42d1c678f0
deiz0n <eduardomacedosilva@outlook.com>
2025-02-09 19:53:12 -0300

    refactor: alterações na nomenclatura das queries

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/HorarioRepository.java
@@ -1,6 +1,7 @@
 package com.deiz0n.studfit.infrastructure.repositories;
 
 import com.deiz0n.studfit.domain.entites.Horario;
+import com.deiz0n.studfit.domain.enums.Turno;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -12,7 +13,9 @@ import java.util.UUID;
 public interface HorarioRepository extends JpaRepository<Horario, UUID> {
 
     @Query("FROM tb_horario h WHERE h.horarioInicial = :start AND h.horarioFinal = :end")
-    Optional<Horario> getHorario(LocalTime start, LocalTime end);
-    List<Horario> getByTurno(String turno);
+    Optional<Horario> buscarHorario(LocalTime start, LocalTime end);
+
+    @Query("FROM tb_horario h WHERE h.turno = :turno")
+    List<Horario> buscarHorariosPorTurno(Turno turno);
 
 }
