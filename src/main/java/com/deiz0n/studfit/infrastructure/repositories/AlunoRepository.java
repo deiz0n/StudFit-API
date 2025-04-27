a4395adead65edc013ddb815d85baad5637390f0
deiz0n <eduardomacedosilva@outlook.com>
2025-04-26 23:06:38 -0300

    refactor: ajustes nas queries buscarAlunoListaEspera e buscarAlunoEfetivado

--- src/main/java/com/deiz0n/studfit/infrastructure/repositories/AlunoRepository.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/AlunoRepository.java
@@ -1,7 +1,6 @@
 package com.deiz0n.studfit.infrastructure.repositories;
 
 import com.deiz0n.studfit.domain.dtos.AlunoEfetivadoDTO;
-import com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO;
 import com.deiz0n.studfit.domain.entites.Aluno;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -22,11 +21,21 @@ public interface AlunoRepository extends JpaRepository<Aluno, UUID> {
     @Query("FROM tb_aluno a WHERE a.telefone = :telefone")
     Optional<Aluno> buscarPorTelefone(String telefone);
 
-    @Query("SELECT NEW com.deiz0n.studfit.domain.dtos.AlunoListaEsperaDTO(a.id, a.nome, a.colocacao, a.turnosPreferenciais) FROM tb_aluno a WHERE a.listaEspera = true")
-    List<AlunoListaEsperaDTO> buscarAlunosListaEspera(Pageable pageable);
+    @Query("SELECT a " +
+            "FROM tb_aluno a " +
+            "LEFT JOIN FETCH a.turnosPreferenciais tp " +
+            "LEFT JOIN FETCH tp.turno t " +
+            "LEFT JOIN FETCH a.horario h " +
+            "ORDER BY a.colocacao")
+    List<Aluno> buscarAlunosListaEspera();
 
-    @Query("SELECT NEW " +
-            "com.deiz0n.studfit.domain.dtos.AlunoEfetivadoDTO(a.id, a.nome, NEW com.deiz0n.studfit.domain.dtos.HorarioDTO(h.horarioInicial, h.horarioFinal, h.turno), a.status)" +
-            " FROM tb_aluno a JOIN FETCH tb_horario h ON a.horario.id = h.id")
+    @Query("SELECT NEW com.deiz0n.studfit.domain.dtos.AlunoEfetivadoDTO(a.id, a.nome, " +
+            "NEW com.deiz0n.studfit.domain.dtos.HorarioDTO(h.horarioInicial, h.horarioFinal, " +
+            "NEW com.deiz0n.studfit.domain.dtos.TurnoDTO(t.id, t.nome)), a.status) " +
+            "FROM tb_aluno a " +
+            "LEFT JOIN tb_horario h " +
+            "ON a.horario.id = h.id " +
+            "LEFT JOIN tb_turno t " +
+            "ON h.turno.id = t.id")
     List<AlunoEfetivadoDTO> buscarAlunosEfetivados(Pageable pageable);
 }
