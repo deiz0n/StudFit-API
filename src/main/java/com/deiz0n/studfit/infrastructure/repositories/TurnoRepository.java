23c1e283dc560113cb8fdac0acebfcb748ad5b95
deiz0n <eduardomacedosilva@outlook.com>
2025-05-05 14:54:41 -0300

    feat: criação da query buscarPorNome

---
+++ src/main/java/com/deiz0n/studfit/infrastructure/repositories/TurnoRepository.java
@@ -0,0 +1,19 @@
+package com.deiz0n.studfit.infrastructure.repositories;
+
+import com.deiz0n.studfit.domain.dtos.TurnoDTO;
+import com.deiz0n.studfit.domain.entites.Turno;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface TurnoRepository extends JpaRepository<Turno, UUID> {
+
+    @Query("SELECT NEW " +
+            "com.deiz0n.studfit.domain.dtos.TurnoDTO(t.id, t.nome) " +
+            "FROM tb_turno t " +
+            "WHERE t.nome ILIKE :nome")
+    Optional<TurnoDTO> buscarPorNome(String nome);
+
+}
