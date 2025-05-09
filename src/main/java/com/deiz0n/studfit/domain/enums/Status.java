da4a504bf9bda1924a66a324f5e61a1f9f12659e
deiz0n <eduardomacedosilva@outlook.com>
2025-05-09 10:55:21 -0300

    feat: adição do status de 'CADASTRO_PENDENTE'

--- src/main/java/com/deiz0n/studfit/domain/enums/Status.java
+++ src/main/java/com/deiz0n/studfit/domain/enums/Status.java
@@ -3,9 +3,13 @@ package com.deiz0n.studfit.domain.enums;
 public enum Status {
 
     REGULAR("regular"),
-    EM_ALERTA("em_alerta");
+    EM_ALERTA("em_alerta"),
+    CADASTRO_PENDENTE("cadastro_pendente");
+
+    private String status;
 
     Status(String status) {
+        this.status = status;
     }
 
 }
