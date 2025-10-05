35a3ab310fc6c9225b3cf12f9219de8a44e383d5
deiz0n <eduardomacedosilva@outlook.com>
2025-10-05 00:23:57 -0300

    feat: adição do método uploadAtestado via aws s3

---
+++ src/main/java/com/deiz0n/studfit/infrastructure/aws/S3Service.java
@@ -0,0 +1,55 @@
+package com.deiz0n.studfit.infrastructure.aws;
+
+import com.deiz0n.studfit.domain.utils.GerarNomeAtestado;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@Service
+public class S3Service {
+
+    private final S3Client s3Client;
+    private final String bucket;
+
+    public S3Service(
+            @Value("${aws.accessKey}") String accessKey,
+            @Value("${aws.secretKey}") String secretKey,
+            @Value("${aws.sessionToken:}") String sessionToken,
+            @Value("${aws.region}") String region,
+            @Value("${aws.s3.bucket}") String bucket
+    ) {
+        this.s3Client = S3Client.builder()
+                .region(Region.of(region))
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsSessionCredentials.create(accessKey, secretKey, sessionToken)
+                        )
+                )
+                .build();
+        this.bucket = bucket;
+    }
+
+    public String uploadAtestado(MultipartFile atestado, UUID alunoId) throws IOException {
+        var nomeAtestado = GerarNomeAtestado.gerarNome(alunoId);
+
+        var putObjectRequest = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(nomeAtestado)
+                .contentType(atestado.getContentType())
+                .build();
+
+        s3Client.putObject(putObjectRequest, RequestBody.fromBytes(atestado.getBytes()));
+
+        return String.format("https://%s.s3.amazonaws.com/", bucket);
+    }
+
+}
