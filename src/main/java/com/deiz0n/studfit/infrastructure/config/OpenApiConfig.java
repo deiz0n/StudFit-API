46e9023b144060aec673ca14a5ea3d569b8b253f
deiz0n <eduardomacedosilva@outlook.com>
2025-12-05 18:52:44 -0300

    feat: adição do JWT token para acesso das rotas privadas no swagger

--- src/main/java/com/deiz0n/studfit/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/config/OpenApiConfig.java
@@ -1,9 +1,12 @@
 package com.deiz0n.studfit.infrastructure.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,7 +15,18 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName, new SecurityScheme()
+                                .name(securitySchemeName)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .description("Insira o token JWT obtido através do endpoint de autenticação")))
                 .info(new Info()
                         .title("StudFit API")
                         .version("1.0")
