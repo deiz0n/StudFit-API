7b5054b0b546de5423271478598e3b856d68f2ce
deiz0n <eduardomacedosilva@outlook.com>
2025-12-13 01:48:05 -0300

    fix: remoção da configuração global de token, agora cada controller implementa sua configuração

--- src/main/java/com/deiz0n/studfit/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/deiz0n/studfit/infrastructure/config/OpenApiConfig.java
@@ -5,7 +5,6 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,8 +17,6 @@ public class OpenApiConfig {
         String securitySchemeName = "bearerAuth";
 
         return new OpenAPI()
-                .addSecurityItem(new SecurityRequirement()
-                        .addList(securitySchemeName))
                 .components(new Components()
                         .addSecuritySchemes(securitySchemeName, new SecurityScheme()
                                 .name(securitySchemeName)
